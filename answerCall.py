e76f4f8a7763b65e60077f28021bb6e00139be30
Hudson_Patrick <hudsp431@hsd.k12.or.us>
2023-10-18 09:47:07 -0700

    finishing code

--- answerCall.py
+++ answerCall.py
@@ -1,14 +1,27 @@
 # answerCall.py
-# by _______
+# by Patrick Hudson
 
 # For instructions on what to do, see README.md
-# or visit (https://github.com/HundredVisionsGuy/answerCall)
+# or visit (https://github.com/HundredVisionsGuy/answerCall) 
 
 # Write function defintion: answerCall()
+def answerCall(caller_code, sameAreaCode, cur_time):
+    response = True
+    hour, minute = cur_time.split(":")
+    if int(hour) < int(7):
+        response = False
+    if int(hour) > int(22):
+        response = False
+    if caller_code == 'T':
+        response = False
+    if caller_code == 'U' and sameAreaCode == False:
+        response = False
+    
+    return response
 
 # Make sure it returns a value
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
     # Make sure it's indented
-    pass # remove or comment out this line if you wish to test the function
+    answerCall("F", False, "13:00")
